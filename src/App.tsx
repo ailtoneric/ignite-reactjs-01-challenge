ccbcfcfe33a6def0f9c8a1cb84f43b9e2d833ed2
Ailton Oliveira <ailton.oliveira@vocacao.org.br>
2023-07-12 10:07:43 -0300

    create input and button in new task

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import { Header } from './components/Header'
+import plusLogo from './assets/plus.svg'
 
+import styles from './App.module.css'
 import './global.css'
 
 export function App() {
   return (
     <div>
       <Header />
-      <h1>Hello World!</h1>
+      <main className={styles.content}>
+        <header className={styles.header}>
+          <input type="text" placeholder='Adicione uma nova tarefa' />
+          <button>
+            Criar
+            <img src={plusLogo} alt="icone de adição" />
+          </button>
+        </header>
+        <div>
+          <p>Você ainda não tem tarefas cadastradas</p>
+          <p>Crie tarefas e organize seus itens a fazer</p>
+        </div>
+      </main>
     </div>
   )
 }
